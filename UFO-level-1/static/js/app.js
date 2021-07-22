f219a6fd3be122e0661d15ae1ce539d3b78bd23a
Ryanwm2000 <80086540+Ryanwm2000@users.noreply.github.com>
2021-07-21 19:04:26 -0700

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,48 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// references and variables creation
+var $tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var Date = d3.select("#datetime");
+var City = d3.select("#city");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+// html imput
+var addData = (dataInput) => {
+    dataInput.forEach(ufoSightings => {
+        var row = $tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSightings[column])
+        )
+    });
+}
+
+addData(tableData);
+
+
+// Event listener 
+button.on("click", () => {
+
+    d3.event.preventDefault();
+    
+var inputDate = Date.property("value").trim();
+  
+    
+var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
+    
+
+    $tbody.html("");
+
+    let response = {
+        filterDate
+    }
+
+
+    if(response.filterDate.length !== 0) {
+        addData(filterDate);
+    }
+
+        else {
+            $tbody.append("tr").append("td").text("No Sightings");
+        }
+})
